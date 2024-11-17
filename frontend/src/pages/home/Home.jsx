a046bf96064587b6550a15cea6ec321d62236934
Krish D <krishnendu.das@siemens.com>
2024-11-17 19:20:00 +0530

    adding front end for book exchange app

---
+++ frontend/src/pages/home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Banner from './Banner'
+import TopSellers from './TopSellers'
+import Recommened from './Recommened'
+import News from './News'
+
+const Home = () => {
+  return (
+    <>
+        <Banner/>
+        <TopSellers/>
+        <News/>
+    </>
+  )
+}
+
+export default Home
